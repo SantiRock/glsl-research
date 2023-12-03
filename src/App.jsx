5977cdd2bca8a7b03d96f7562466fa8d0c218aca
Santiago Quintero <santiago.qp86@gmail.com>
2023-12-03 01:54:41 -0500

    instrucciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import styles from './App.module.css';
 import { Gallery } from './components/gallery/gallery';
+import { About } from './components/about/about';
 
 
 function App() {
   return (
     <div class={styles.container}>
       <Gallery />
+      <About />
     </div>
   );
 }
---
+++ src/components/about/about.jsx
@@ -0,0 +1,9 @@
+export function About() {
+    return (
+        <>
+            <p>Dale click a la imagen y, sosteniendo el click, mueve el mouse</p>
+            <p>Click on the image and, holding the click, move the mouse</p>
+            <p>Cliquez sur l'image et, en maintenant le click, déplacez la souris</p>
+        </>
+    )
+}
