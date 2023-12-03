30b78d3a30fae3b5b153918cf30520cfdc2b7ee0
Santiago Quintero <santiago.qp86@gmail.com>
2023-12-03 01:59:06 -0500

    instrucciones

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,7 +3,7 @@ export function About() {
         <>
             <p>Dale click a la imagen y, sosteniendo el click, mueve el mouse</p>
             <p>Click on the image and, holding the click, move the mouse</p>
-            <p>Cliquez sur l'image et, en maintenant le click, déplacez la souris</p>
+            <p>Cliquez sur l'image et, en le laissant appuyé, déplacez la souris</p>
         </>
     )
 }
