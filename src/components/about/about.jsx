0cf39efdc0d98a8c3f122b1d9383f7e05ad60bd6
santirockk <santiquu@gmail.com>
2024-02-23 11:23:56 -0500

    centrado

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -19,7 +19,7 @@ export function About() {
                 <p>Dale click a la imagen y, sosteniendo el click, mueve el mouse</p>
                 <p>Click on the image and, holding the click, move the mouse</p>
                 <p>Cliquez sur l'image et, en le laissant appuyé, déplacez la souris</p>
-                <p class={style.weigth}>This website is 51Ko</p>
+                <p class={style.weigth}>This website is 51Ko ❃ႣᄎႣ❃</p>
             </Show>
         </div>
     )
